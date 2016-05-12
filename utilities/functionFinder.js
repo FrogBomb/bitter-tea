df4d065b41d5348aa0c5e1df263a6e5143ab0d0a
FrogBomb <TLBlanchet@gmail.com>
2016-05-12 14:10:11 -0600

    Rework of findFunctionNames
    reworked the regexp for finding function names.

--- utilities/functionFinder.js
+++ utilities/functionFinder.js
@@ -6,15 +6,21 @@ function findFunctionNames(codestr){
         codestr: A string of javascript code
         
     OUTPUT:
-        An array of the function names from the input string. WILL NOT FIND ANONYMOUS FUNCTIONS!
+        An array of the function names from the input string. WILL NOT FIND ANONYMOUS FUNCTIONS
+        OR FUNCTIONS INSIDE ANOTHER FUNCTION CONTEXT!
     */
-    var funcRE = /(^\s*function\s*)[\w]+(?=\()/g;
+    var funcRE = /^(?:[^\{(?!\})]\s*function\s*)[\w]+(?=\()/g;
     return funcRE.match(codestr); 
 }
 
 function findFunctions(codestr){
+    /*
+        Returns an object with keys to the functions defined in codestr
+    */
+    
 }
 
 module.exports = {
-    findFunctionNames: findFunctionNames
+    findFunctionNames   : findFunctionNames,
+    findFunctions       : findFunctions
 }
