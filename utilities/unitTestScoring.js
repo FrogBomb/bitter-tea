f2e018837780eaa1d6db2f7a90a228d9a25207c6
FrogBomb <TLBlanchet@gmail.com>
2016-05-12 12:49:35 -0600

    Some shit I did with unitTestScoring
    Fleshed out the unitTestScoring utility. Should work, but need to test.
    Also, built for Chrome only atm. '

--- utilities/unitTestScoring.js
+++ utilities/unitTestScoring.js
@@ -1,4 +1,4 @@
-module.exports = function(functionToTest, comp){
+var unitTestScoring = function(functionToTest, comp){
 /*
     PARAMETERS:
         functionToTest      : <function: input -> output >
@@ -8,19 +8,41 @@ module.exports = function(functionToTest, comp){
             A function to compare the desired outputs to the given outputs of the function.
             
     OUTPUT:
-        <OUTPUT>            : <function: (input, desiredOut, successScore, failureScore) -> null> 
+        <OUTPUT>            : <function: (input, desiredOut, successScore, failureScore) -> boolean > 
             A function for unit testing that keeps a score in it's own parameter
-            input will be an array of the input arguements.
+            input will be an array of the input arguements. The output will be a boolean: true if passed
+            false if the test fails. 
             
         <OUTPUT>.score      : numeric
             Parameter of the output that keeps a score. Initially will be 0.
+            
+            
+    STATIC METHODS:
+        unitTestScoring
+            .getTotalScore      : < function () -> int >
+                Return the total score of all unit tests.
+            
+            ._tests              : Array of <OUTPUT>s
+                Holds all tester functions generated so far. (Private)
             
 */
     if(!comp){
         comp = function(a, b){return a === b;};
     }
     var retFunction = function(input, desiredOut, successScore, failureScore){
-        retFunction.score += comp(functionToTest(...input), desiredOut) ? successScore : failureScore;
+        var out = functionToTest(...input);
+        var testWorked = comp(out, desiredOut);
+        retFunction.score += testWorked ? successScore : failureScore;
+        return testWorked;
     };
     retFunction.score = 0;
+    unitTestScoring.tests.push(retFunction);
 };
+
+unitTestScoring._tests = [];
+
+unitTestScoring.getTotalScore = function(){
+    return unitTestScoring.tests.reduce(function(acc, val){return acc+val.score;}, 0);
+};
+
+module.exports = unitTestScoring;
