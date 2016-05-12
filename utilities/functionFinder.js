76736be87f99f305575ddd585fcbb48fc4b09f47
FrogBomb <TLBlanchet@gmail.com>
2016-05-12 14:17:13 -0600

    Oh god why findFunctions..
    I found the functions, and they were good. Also, regexp is even better
    than before!?

--- utilities/functionFinder.js
+++ utilities/functionFinder.js
@@ -9,16 +9,24 @@ function findFunctionNames(codestr){
         An array of the function names from the input string. WILL NOT FIND ANONYMOUS FUNCTIONS
         OR FUNCTIONS INSIDE ANOTHER FUNCTION CONTEXT!
     */
-    var funcRE = /^(?:[^\{(?!\})]\s*function\s*)[\w]+(?=\()/g;
+    var funcRE = /^(?:[^\{(?!\})]\s*function\s*)[\w]+\s*(?=\()/g;
     return funcRE.match(codestr); 
 }
 
-function findFunctions(codestr){
+function findFunctions(codestr){//The worst code ever. 
     /*
         Returns an object with keys to the functions defined in codestr
     */
-//    eval(codestr);
-    
+    var _________retObj = {};
+    var fNames = findFunctionNames(codestr);
+    for(var i = 0; i<fName.length; i++){
+        _________retObj[fNames[i]] = undefined;
+    }
+    eval(codestr);
+    for(var __keyValuesInRet in _________retObj){
+        _________retObj[__keyValuesInRet] = eval(__keyValuesInRet);
+    }
+    return _________retObj;
 }
 
 module.exports = {
