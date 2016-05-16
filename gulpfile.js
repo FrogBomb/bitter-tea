410e5df0657af53dc9f06ae6f45ab6875f609906
kpreucil <k.preucil@lavishcraft.com>
2016-05-16 10:27:31 -0600

    added watch, reorganized file
    changed name of html doc not in html folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,25 @@ var gulpIf = require('gulp-if');
 var del = require('del');
 var useref = require('gulp-useref');
 
-gulp.task('useref',['clean'], function(){
-  return gulp.src('html/*.html')
-    .pipe(useref())
-    // Minifies only if it's a JavaScript file
-//    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulp.dest('dist'))
+gulp.task('useref', ['clean'], function () {
+	return gulp.src('html/*.html')
+		.pipe(useref())
+		// Minifies only if it's a JavaScript file
+		//    .pipe(gulpIf('*.js', uglify()))
+		.pipe(gulp.dest('dist'))
 });
 
-gulp.task('clean', function(){
-    return del('./dist/**');
+gulp.task('clean', function () {
+	return del('./dist/**');
 });
 
 gulp.task('default', ['useref']);
+
+gulp.task(
+	'watch',
+	function () {
+		watch('./**/*', function (events) {
+			gulp.start('default');
+		});
+	}
+);
